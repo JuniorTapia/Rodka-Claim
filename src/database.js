5f8edb94fa493ece42c8c269306063b207f7411b
Junior <junior_tapia_sonic@hotmail.com>
2020-09-29 16:18:10 -0500

    Subida 1

--- src/database.js
+++ src/database.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://junior:junior123>@proyectobd3.l4goe.mongodb.net/pbd3?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://junior:junior123@proyectobd3.l4goe.mongodb.net/pbd3?retryWrites=true&w=majority',{
     //ponemos configuraciones para que el modulo si se actualiza no haya problemas cuando estes haciendo consultas a tu bd 
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 }).then(db=> console.log('Se conecto a la BD Mongo')).catch(err => console.error(err));
+
+/* mongodb+srv://junior:junior123@proyectobd3.l4goe.mongodb.net/pbd3?retryWrites=true&w=majority
+mongodb://localhost/SLA
+ */
